4ac4867e4e8c3964afa3376dcb449a6965c83e6f
VietStrategy <victor.vu1906@gmail.com>
2020-06-23 20:24:14 +0700

    add schema

--- app/etc/config.php
+++ app/etc/config.php
@@ -241,6 +241,7 @@ return [
         'Amazon_Payment' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
+        'Foggyline_Office' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
         'Klarna_Kp' => 1,
